1068279094a1acbc6bcd40522730e90ae8323367
v.surguch <v.surguch@mail.ru>
2021-01-03 17:10:44 +0300

    config.py removed

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ INSTANCE = path.dirname(__file__)
 USERS = {308634083,}
 TOKEN = "495650995:AAHM6ffVVPXrbyjOr3lhgVo2IbHScSZWkmY"
 NAME = "bot"
-DB_FILENAME = "data/reminder.sqlite"
+DB_FILENAME = path.join(INSTANCE, "data/reminder.sqlite")
